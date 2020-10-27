6e6918ab85ea4adcd5fa28f5eaeccc278d6c5da7
Dongjin Lee <djlee1509@gmail.com>
2020-10-27 00:48:31 +0000

    Adding a method to read text file and return an array of URLs.

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
+require './lib/image_fetcher.rb'
+
+text_file = './sample/url.txt'
+
+imagefetcher = ImageFetcher.new
+p imagefetcher.read_file(text_file)
+
--- lib/image_fetcher.rb
+++ lib/image_fetcher.rb
@@ -1,2 +1,18 @@
+require 'open-uri'
+
+
 class ImageFetcher
+
+  # Read Text file and return list of URLs in an array.
+  def read_file(text_file)
+    url_list = []
+    File.open(text_file, "r") do |f|
+      f.each_line do |line|
+        url_list.append(line.delete("\n"))
+      end
+    end
+
+    return url_list
+  end
+
 end
