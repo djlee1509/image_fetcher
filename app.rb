ab39c897eed08b3119047433e008d71906bde7ca
Dongjin Lee <djlee1509@gmail.com>
2020-10-27 22:54:55 +0000

    Added the logic to take argument in command line script.

--- app.rb
+++ app.rb
@@ -6,4 +6,13 @@ require './lib/image_fetcher.rb'
 text_file = './sample/url.txt'
 
 imagefetcher = ImageFetcher.new
-imagefetcher.main(text_file)
+
+
+if ARGV.length == 1
+  puts "Script "
+  text_file = ARGV[0]
+  imagefetcher.fetch_image(text_file)
+  exit
+else
+  puts "#{ARGV.length} arguments provided. Requires only one argument, plain text file which contains the list of URLs."
+end
